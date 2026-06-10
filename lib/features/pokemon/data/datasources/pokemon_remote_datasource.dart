de518909c055028ee20b19acdb3c8e8eca0dfc9e
Aline Alflen <alflenaline@gmail.com>
2026-06-10 18:06:43 -0300

    feat: implement repository layer with clean architecture pattern

---
+++ lib/features/pokemon/data/datasources/pokemon_remote_datasource.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+
+class PokemonRemoteDataSource {
+  final Dio dio;
+
+  PokemonRemoteDataSource(this.dio);
+
+  Future<List<dynamic>> getPokemons() async {
+    try {
+      final response = await dio.get('/pokemon?limit=20');
+      return response.data['results'];
+    } on DioException catch (e) {
+      throw _handleDioError(e);
+    }
+  }
+
+  Future<Map<String, dynamic>> getPokemonDetail(String url) async {
+    try {
+      final response = await dio.get(url);
+      return response.data;
+    } on DioException catch (e) {
+      throw _handleDioError(e);
+    }
+  }
+
+  Exception _handleDioError(DioException e) {
+    if (e.type == DioExceptionType.connectionTimeout) {
+      return Exception('Timeout ao conectar com servidor');
+    }
+
+    if (e.type == DioExceptionType.receiveTimeout) {
+      return Exception('Tempo de resposta excedido');
+    }
+
+    if (e.response != null) {
+      return Exception('Erro na API: ${e.response?.statusCode}');
+    }
+
+    return Exception('Erro desconhecido');
+  }
+}
---
+++ lib/features/pokemon/data/repositories/pokemon_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:pokestore/features/pokemon/data/datasources/pokemon_remote_datasource.dart';
+import 'package:pokestore/features/pokemon/data/models/pokemon_model.dart';
+import 'package:pokestore/features/pokemon/domain/repositories/pokemon_repository.dart';
+import 'package:pokestore/features/pokemon/domain/entities/pokemon.dart';
+
+class PokemonRepositoryImpl implements PokemonRepository {
+  final PokemonRemoteDataSource dataSource;
+
+  PokemonRepositoryImpl(this.dataSource);
+
+  @override
+  Future<List<Pokemon>> getPokemons() async {
+    final results = await dataSource.getPokemons();
+
+    List<Pokemon> pokemons = [];
+
+    for (final item in results) {
+      final detail = await dataSource.getPokemonDetail(item['url']);
+
+      final model = PokemonModel.fromJson(detail);
+
+      pokemons.add(Pokemon(id: model.id, name: model.name, image: model.image));
+    }
+
+    return pokemons;
+  }
+}
