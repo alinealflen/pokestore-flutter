567da130dd14889debd3f26aeacf8a257d8ec953
Aline Alflen <alflenaline@gmail.com>
2026-06-11 14:56:02 -0300

    feat: configure dependency injection for pokemon feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:pokestore/features/pokemon/data/datasources/pokemon_remote_datasource.dart';
+import 'package:pokestore/features/pokemon/data/repositories/pokemon_repository_impl.dart';
+import 'package:pokestore/features/pokemon/domain/usecases/get_pokemons.dart';
+import 'package:pokestore/features/pokemon/presentation/providers/pokemon_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://pokeapi.co/api/v2',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    ),
+  );
+
+  final dataSource = PokemonRemoteDataSource(dio);
+  final repository = PokemonRepositoryImpl(dataSource);
+  final getPokemons = GetPokemons(repository);
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => PokemonProvider(getPokemons)..fetchPokemons(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
